3a0ced351087f1c3309bb4a1a5f7247849cbadea
BaslielZ <baslielz727@gmail.com>
2025-09-24 15:30:36 +0300

    Saying Hello

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSayHelloClicked(object sender, EventArgs e)
+        {
+            string name = MyName.Text;
+            MyLabel.Text = $"Hello, {name}.";
+        }
+    }
+}
